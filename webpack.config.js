eef153286e34a3f48cccd4adadc1b289e7f77050
Harby <harby250@gmail.com>
2021-07-23 19:22:36 -0500

    fixed clean webpack production

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const { cleanWebpackPlugin } = require('clean-webpack-plugin').CleanWebpackPlugin;
+const { cleanWebpackPlugin, CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 
 
@@ -57,7 +57,7 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: '[name].css'
         }),
-        new cleanWebpackPlugin()
+        new CleanWebpackPlugin();
     ],
     optimization: {
         minimize: true,
